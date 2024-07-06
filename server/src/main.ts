225b3b7cb48d57c3a41c55294e2d26d8bdb65937
schenok <142044760+gladyshewm@users.noreply.github.com>
2024-07-06 14:29:43 +0300

    feat: enable CORS

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,14 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT || 5000;
+  const corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200,
+    credentials: true,
+  };
+
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors(corsOptions);
   await app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 }
 bootstrap();
