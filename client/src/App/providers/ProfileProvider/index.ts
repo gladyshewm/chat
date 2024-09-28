e266d704f1d357d30b3f93a6766cc86bb13580c9
schenok <142044760+gladyshewm@users.noreply.github.com>
2024-09-28 15:12:33 +0300

    Delete client/src/App phantom directory

--- client/src/App/providers/ProfileProvider/index.ts
+++
@@ -1,1 +0,0 @@
-export { ProfileProvider } from './ProfileProvider';
--- client/src/App/providers/hooks/useAuth.ts
+++
@@ -1,6 +0,0 @@
-import React, { useContext } from 'react';
-import { AuthContext } from '../AuthProvider';
-
-export const useAuth = (): React.ContextType<typeof AuthContext> => {
-  return useContext(AuthContext);
-};
